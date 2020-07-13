38c1e7c2d0f389a453bbbd297dae75551af9da08
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-07-13 21:12:58 +0530

    Intial Commit of infrastructure dependency project

---
+++ src/MicroServicesRabbitMQ.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroServicesRabbitMQ.Bus;
+using MicroServicesRabbitMQ.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServicesRabbitMQ.Infra.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domaind Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
