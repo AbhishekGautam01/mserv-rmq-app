bd4db3e93d51e90116ae7238cc9511fbf3f4c406
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-07-13 21:39:14 +0530

    Added required Nuget package for EF Core Added DbContextClass

---
+++ src/MicroServices.RabbitMQ.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MicroServices.RabbitMQ.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.RabbitMQ.Banking.Data.Context
+{
+    public class BankingDbContext: DbContext
+    {
+        public BankingDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
