2129c00d05ca2502671a1a39802c248ec19f5b83
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-07-13 21:55:28 +0530

    Added account service

---
+++ src/MicroServices.RabbitMQ.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,23 @@
+﻿using MicroServices.RabbitMQ.Banking.Application.Interfaces;
+using MicroServices.RabbitMQ.Banking.Data.Repository;
+using MicroServices.RabbitMQ.Banking.Domain.Interfaces;
+using MicroServices.RabbitMQ.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.RabbitMQ.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepositary;
+        public AccountService(IAccountRepository accountRepository)
+        {
+            _accountRepositary = accountRepository;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepositary.GetAccounts();
+        }
+    }
+}
---
+++ src/MicroServices.RabbitMQ.Banking.Data/Repository/AccountRepositary.cs
@@ -0,0 +1,22 @@
+﻿using MicroServices.RabbitMQ.Banking.Data.Context;
+using MicroServices.RabbitMQ.Banking.Domain.Interfaces;
+using MicroServices.RabbitMQ.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServices.RabbitMQ.Banking.Data.Repository
+{
+    public class AccountRepositary : IAccountRepository
+    {
+        private BankingDbContext _ctx;
+        public AccountRepositary(BankingDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _ctx.Accounts;
+        }
+    }
+}
