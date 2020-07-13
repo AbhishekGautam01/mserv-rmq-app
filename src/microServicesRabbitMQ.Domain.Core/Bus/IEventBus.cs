1608e91a21c4c57bed14a6adb6b739ad5396c10e
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-07-13 19:27:16 +0530

    Implemented Interfaces and classes for Domain Core Entities

---
+++ src/microServicesRabbitMQ.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using MicroServicesRabbitMQ.Domain.Core.Commands;
+using MicroServicesRabbitMQ.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroServicesRabbitMQ.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T Command) where T : Command;
+
+        void Publish<T>(T @event) where T: Event;
+        void Subscribe<T, THandler>() where T : Event where THandler : IEventHandler<T>;
+    }
+}
