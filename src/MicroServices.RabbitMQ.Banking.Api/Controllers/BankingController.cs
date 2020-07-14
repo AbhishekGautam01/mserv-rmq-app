1e1425f64f4d1a860e75a67e374fbeac8c62d10c
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-07-14 15:25:42 +0530

    Partially Completed Banking Micro Service

---
+++ src/MicroServices.RabbitMQ.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroServices.RabbitMQ.Banking.Application.Interfaces;
+using MicroServices.RabbitMQ.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServices.RabbitMQ.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+
+    }
+}
--- src/MicroServices.RabbitMQ.Banking.Api/Startup.cs
+++ src/MicroServices.RabbitMQ.Banking.Api/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
+using MicroServices.RabbitMQ.Banking.Data.Context;
+using MicroServicesRabbitMQ.Infra.IOC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,8 +29,21 @@ namespace MicroServices.RabbitMQ.Banking.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("bankingDBConnection"));
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking MicroService", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
+            services.AddControllers();
+            RegisterServices(services);
         }
+
+        private void RegisterServices(IServiceCollection services) => DependencyContainer.RegisterServices(services);
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -42,7 +59,21 @@ namespace MicroServices.RabbitMQ.Banking.Api
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "Banking Microservice v1");
+            });
+
         }
     }
 }
